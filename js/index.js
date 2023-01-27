44740d2d63a9e0c6cabdf125f255ce27bcdd7767
Ana Carla <anacarlaa16@gmail.com>
2023-01-27 20:18:56 -0300

    refactored

--- js/index.js
+++ js/index.js
@@ -18,27 +18,22 @@ function createList(array) {
 createList(data);
 
 function createCards(object){
-  
   const img = document.createElement("img");
   const divImg = document.createElement("div");
-
   const category = document.createElement("p");
   const name = document.createElement("h3");
   const description = document.createElement("p");
   const price = document.createElement("p");
   const button = document.createElement("button");
-
   const content = document.createElement("div");
   const li = document.createElement("li");
 
-  img.src = `${object.img}`;
+  img.src = object.img;
+  divImg.appendChild(img);
   category.innerText = object.tag;
   name.innerText = object.nameItem;
   description.innerText = object.description;
-  price.innerText = object.value.toLocaleString("pt-br", {
-    style: "currency",
-    currency: "BRL",
-  });
+  price.innerText = object.value.toLocaleString("pt-br", {style: "currency", currency: "BRL"});
   button.innerText = object.addCart;
 
   divImg.classList.add("div-img");
@@ -46,30 +41,19 @@ function createCards(object){
   name.classList.add("product-name");
   description.classList.add("product-description");
   price.classList.add("product-price");
-  button.classList.add("add-to-cart-button");
-
   content.classList.add("card-content");
   li.classList.add("card-style");
+  button.classList.add("add-to-cart-button");
 
   button.addEventListener("click", function () {
-    const search = cartData.find((elem) => elem.id === object.id)
-    
-   if(!search){
-      filtered.push(object)
-      cartData.push(object)
-      sum(cartData) 
-     return createMiniCards(filtered, object, 1)
-   }
-      cartData.push(object)
-      createMiniCards(filtered,object,1)
-      sum(cartData);
-     
+    const search = cartData.find(elem => elem.id === object.id);
+    if(!search) filtered.push(object);
+    cartData.push(object);
+    sum(cartData);
+    return createMiniCards(filtered, object, 1);
   });
-
-  divImg.append(img);
-  content.append(category, name, description, price);
-  li.append(divImg, content, button);
-
+  
+  li.append(divImg, category, name, description, price, button);
   return li;
 }
 
@@ -78,7 +62,6 @@ function sum(arr) {
   const total = arr.forEach((elem) => (valor += elem.value));
   createTotal(valor)
 }
-
 
 function createTotal(value) {
   if(value === 0){
@@ -100,89 +83,44 @@ function createTotal(value) {
   cartSession.append(text, total)
 
 }
-
-function createMiniCards(cart, object, key){
-
+function createMiniCards(cart, object, key) {
   const ul = document.createElement("ul");
-  ul.innerHTML = "";
-  cartDiv.innerHTML = "";
   ul.classList.add("cart-ul", "radius");
-
   cart.forEach((elem) => {
-    const trash = document.createElement("img");
-    const price = document.createElement("p");
-    const quantity = document.createElement("p");
-    const div2 = document.createElement("div");
-    const name = document.createElement("p");
-    const div1 = document.createElement("div");
-    const img = document.createElement("img");
     const li = document.createElement("li");
-
-    img.src = `${elem.img}`;
-    name.innerHTML = elem.nameItem;
-    price.innerHTML = elem.value.toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    });
-    
-    trash.src = "./img/trash.png";
-   
-    if (key === 1 && object.id === elem.id){
-    
-      elem.repeat = elem.repeat + 1
-      quantity.innerHTML = elem.repeat
-      
-      quantity.innerHTML = elem.repeat
-    }
-    else{
-      quantity.innerHTML = elem.repeat
-    }
-    
-
     li.classList.add("cart-li");
-    img.classList.add("img-cart-card");
-    div1.classList.add("cart-card-content");
-    name.classList.add("title1");
-    div2.classList.add("price-and-trash");
-    price.classList.add("price");
-    quantity.classList.add("quantity");
-
-    trash.addEventListener("click", function (event) {
-      const button = event.target
-      //const li = button.closest("li")
-      console.log(button)
-      if(elem.repeat === 1){
-        elem.repeat = 0
-        
-        const index = filtered.findIndex((el) => el.id === elem.id);
-          filtered.splice(index, 1);
-
-        const index2 = cartData.findIndex((el) => el.id === elem.id);
-          cartData.splice(index2, 1);
-         
-      
-      sum(cartData);
-      createMiniCards(filtered, filtered[0],0)
-      }
-      else{
-        
-        elem.repeat--
-        quantity.innerHTML = elem.repeat
-        const index2 = cartData.findIndex((el) => el.id === elem.id);
-        cartData.splice(index2, 1);
-         
-      sum(cartData);
-      }
-      
-    });
-
-    div2.append(price, quantity, trash);
-    div1.append(name, div2);
-    li.append(img, div1);
-
+    li.innerHTML = `
+    <img class="img-cart-card" src="${elem.img}">
+    <div class="cart-card-content">
+      <p class="title1">${elem.nameItem}</p>
+      <div class="price-and-trash">
+        <p class="price">${elem.value.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}</p>
+        <p class="quantity">${key === 1 && object.id === elem.id ? (elem.repeat += 1) : elem.repeat}</p>
+        <img src="./img/trash.png" onclick="removeItem(event, ${elem.id}, ${elem.repeat})">
+      </div>
+    </div>
+    `;
     ul.appendChild(li);
-    cartDiv.appendChild(ul);
   });
+  cartDiv.innerHTML = "";
+  cartDiv.appendChild(ul);
+}
+
+function removeItem(event, id, repeat) {
+  if (repeat === 1) {
+    const index = filtered.findIndex((el) => el.id === id);
+    filtered.splice(index, 1);
+    const index2 = cartData.findIndex((el) => el.id === id);
+    cartData.splice(index2, 1);
+    sum(cartData);
+    createMiniCards(filtered, filtered[0], 0);
+  } else {
+    const elem = filtered.find((el) => el.id === id);
+    elem.repeat--;
+    const index2 = cartData.findIndex((el) => el.id === id);
+    cartData.splice(index2, 1);
+    sum(cartData);
+  }
 }
 
 // SECTIONS 
@@ -221,28 +159,28 @@ const searchError = () =>{
 
 const formDesktop = document.querySelector(".form")
 const formMobile = document.querySelector(".form-mobile")
+const inputs = document.querySelectorAll('.input, .input-mobile');
 
-const inputDesktop = document.querySelector(".input")
-const inputMobile = document.querySelector(".input-mobile")
+inputs.forEach(input => {
+input.addEventListener("submit", event => {
+event.preventDefault();
+callbackSearch(event.target);
+});
+});
 
-formDesktop.addEventListener("submit", (event)=>{
-  event.preventDefault()
-  callbackSearch(formDesktop)
-})
-formMobile.addEventListener("submit", (event)=>{
-  event.preventDefault()
-  callbackSearch(formMobile)
-})
 function callbackSearch(form) {
-  const values = [...form]
-  const inputValue = values[0].value
-  if(!inputValue){
-    return createList(data)
-  }
-  const filter = data.filter(
-  (elem) => elem.nameItem.toLowerCase().includes(inputValue) || elem.tag[0].toLowerCase().includes(inputValue))
-  if(!filter || !filter[0]){
-    return searchError()
-  }
-  return createList(filter)
+const inputValue = form.value;
+if(!inputValue){
+return createList(data);
 }
+const filter = data.filter(elem => elem.nameItem.toLowerCase().includes(inputValue) || elem.tag[0].toLowerCase().includes(inputValue));
+if(!filter || !filter[0]){
+return searchError();
+}
+return createList(filter);
+}
+
+
+
+
+
